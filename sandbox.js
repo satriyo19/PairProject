2171678c396e639bc1fe75ca11e7d6d4493ab1be
satriyo laksono <laksonosatriyo@gmail.com>
2022-10-13 13:25:20 +0700

    process nodemailer

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,47 @@
-var bcrypt = require('bcryptjs');
-var salt = bcrypt.genSaltSync(3);
-var hash = bcrypt.hashSync("konkon", salt);
+/// bycrypt
+// let bcrypt = require('bcryptjs');
+// let salt = bcrypt.genSaltSync(3);
+// let hash = bcrypt.hashSync("konkon", salt);
 
-console.log(hash)
+// console.log(hash)
 
-console.log(bcrypt.compareSync("B4c0/\/", hash))
-console.log(bcrypt.compareSync("konkon", hash))
+// console.log(bcrypt.compareSync("B4c0/\/", hash))
+// console.log(bcrypt.compareSync("konkon", hash))
+
+
+// nodemailer
+let nodemailer = require('nodemailer');
+const senderMail = "socialku69@yahoo.com";
+
+const emailTransporter = nodemailer.createTransport({
+            host: 'smtp.mail.yahoo.com',
+            port: 465,
+            service:'yahoo',
+            secure: false,
+            auth: {
+               user: senderMail,
+               pass: 'as12!@AS'
+            },
+            debug: false,
+            logger: true   //<---highly recommend this one here
+});
+
+// let transporter = nodemailer.createTransport({
+//  service: 'gmail',
+//  auth: {
+//         user: 'socialku69@gmail.com',
+//         pass: 'as12!@AS'
+//     }
+// });
+const mailOptions = {
+  from: 'socialku69@yahoo.com', // sender address
+  to: 'laksonosatriyo@gmail.com', // list of receivers
+  subject: 'Subject of your email', // Subject line
+  html: '<p>kelazzz</p>'// plain text body
+};
+emailTransporter.sendMail(mailOptions, function (err, info) {
+   if(err)
+     console.log(err)
+   else
+     console.log(info);
+});
