90f11ae0e28f06d368fd1226dd0578289d14503e
satriyo laksono <laksonosatriyo@gmail.com>
2022-10-13 09:47:59 +0700

    nambah email

---
+++ migrations/20221013023314-AddEmailForUser.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const { DataTypes } = require('sequelize');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return queryInterface.addColumn('Users', 'email', {type: DataTypes.STRING})
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return queryInterface.removeColumn('Users', 'email', {})
+  }
+};
